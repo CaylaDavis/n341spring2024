8f5c5cbcd4bb92d68d506a3d5cf421dec20802ef
CaylaDavis <137857442+CaylaDavis@users.noreply.github.com>
2024-01-15 23:29:58 -0500

    Newest html

--- gettingstarted.js
+++ gettingstarted.js
@@ -1,16 +1,16 @@
 /*
 TITLE: Getting Started 
 AUTHOR: Macayla C. Davis (MCD)
-CREATE DATE: 27 August 2023
-PURPOSE: To publish first web page for N341
-LAST MODIFIED ON: 27 August 2023
+CREATE DATE: 15 January 2024
+PURPOSE: To send visible output to first web page for N341
+LAST MODIFIED ON: 15 January 2024
 LAST MODIFIED BY: Macayla C. Davis (MCD)
 MODIFICATION HISTORY:
 No modifications 
 */
 
 let myName = document.getElementById("userName")
-let myOrgin = document.getElementById("userOrgin")
+let myQuestion = document.getElementById("userQuestion")
 
 myName.innerHTML = "My name is Macayla Davis."
-myOrgin.innerHTML = "I'm from Indianapolis."
+myQuestion.innerHTML = "I added this class around 10pm on Sunday, January 14th and the canvas page wasn't immediately available. Can I please be excused from the late penalty?"
